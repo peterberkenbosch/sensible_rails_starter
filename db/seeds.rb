f217f11035f34f2a775d680a9482f8eda6199cbe
davidteren <drteren@gmail.com>
2022-07-21 19:14:52 +0100

    add seeds for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# # Seed example user
+# Seed example user
 p User.where(email: "user@example.com")
   .first_or_create!(email: "user@example.com", password: "pass123", confirmed_at: Time.now.utc)
-#
-# # Seed admin user
+
+# Seed admin user
 p User.where(email: "admin@example.com")
   .first_or_create!(email: "admin@example.com", password: "pass123", admin: true, confirmed_at: Time.now.utc)
