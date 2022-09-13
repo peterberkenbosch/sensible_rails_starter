60dc66c1231f35858992d10a0918fff4bb75b68d
davidteren <davidteren@users.noreply.github.com>
2022-09-13 23:05:42 +0200

    Simpletails fixes (#44)
    Simpletails fixes (#44)

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,55 +6,54 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
   ],
   theme: {
+    fontFamily: {
+      'sans': ["BlinkMacSystemFont", "Avenir Next", "Avenir",
+        "Nimbus Sans L", "Roboto", "Noto Sans", "Segoe UI", "Arial", "Helvetica",
+        "Helvetica Neue", "sans-serif"],
+      'mono': ["Consolas", "Menlo", "Monaco", "Andale Mono", "Ubuntu Mono", "monospace"]
+    },
     extend: {
-      fontSize: {
-        'xs': '.75rem',
-        'sm': '.875rem',
-        'tiny': '.875rem',
-        'base': '0.96rem',
-        'lg': '1.15rem',
-        'xl': '1.25rem',
-        '2xl': '1.44rem',
-        '3xl': '2rem',
-        '4xl': ['2.6rem', '1.83em'],
-        '5xl': ['3rem', '1.1'],
-        '6xl': '4rem',
-        '7xl': '5rem',
-      },
+      //  fontSize: {
+      //   'xs': '.75rem',
+      //   'sm': '.875rem',
+      //   'tiny': '.875rem',
+      //   'base': '0.96rem',
+      //   'lg': '1.15rem',
+      //   'xl': '1.25rem',
+      //   '2xl': '1.44rem',
+      //   '3xl': '2rem',
+      //   '4xl': ['2.6rem', '1.83em'],
+      //   '5xl': ['3rem', '1.1'],
+      //   '6xl': '4rem',
+      //   '7xl': '5rem',
+      // },
+
       colors: {
-
-        bg: '#212121',
-       'accent-bg': '#2b2b2b',
-        text: '#dcdcdc',
-        'text-light': '#ababa',
-        border: '#666',
-        accent: '#ffb300',
-        code: '#f06292',
-        preformatted: '#ccc',
-        disabled: '#111',
-
-     /*   bg: 'rgb(var(--bg) / <alpha-value>)',
-        'accent-bg': 'rgb(var(--accent-bg) / <alpha-value>)',
-        text: 'rgb(var(--text) / <alpha-value>)',
-        'text-light': '(var(--text-light) / <alpha-value>)',
-        border:  '(var(--border) / <alpha-value>)',
-        accent:  '(var(--accent) / <alpha-value>)',
-        code:  '(var(--code) / <alpha-value>)',
-        preformatted:  '(var(--pre) / <alpha-value>)',
-        disabled:  '(var(--disabled) / <alpha-value>)',*/
+        simple: {
+          bg: '#212121',
+          'accent-bg': '#2b2b2b',
+          text: '#dcdcdc',
+          'text-light': '#ababab',
+          border: '#666',
+          accent: '#ffb300',
+          code: '#f06292',
+          preformatted: '#ccc',
+          disabled: '#111',
+          marked: "#11110DFF"
+        },
       },
     },
   },
   corePlugins: {
-    aspectRatio: false
+    aspectRatio: false,
   },
   plugins: [
     require('@tailwindcss/typography'),
-    
+    require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/line-clamp')
-  ]
+    require('@tailwindcss/line-clamp'),
+  ],
 }
